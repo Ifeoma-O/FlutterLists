5886df5b466b1cb1e1e906bed526fda1eec5e357
Ifeoma <ifyokpara01@gmail.com>
2024-03-11 22:59:13 +0100

    Updated version

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -6,20 +6,45 @@ class MainScreen extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main Screen'),
+        title: const Text('Family'),
       ),
       body: ListView(
         children: [
-          ListTile(
-            leading: Icon(Icons.nature),
-            title: Text('Nature'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.push(
-                context, 
-                MaterialPageRoute(builder: (context) => OtherScreen())
-              );
-            },
+          Container(
+            margin: EdgeInsets.all(10),
+            width: 200,
+            height: 140,
+            decoration:BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(width: 1, color: Colors.blue),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Padding(
+                  padding:  EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/shuga.jpg'),
+                    radius: 80,
+                  ),
+                ),
+                Text('My shuga mommy',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'KumbhSans'
+                  )
+                ), 
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.white,
+                  ),
+                ), 
+              ],
+            ),
           ),
           ListTile(
             leading: Icon(Icons.wb_auto),
